07ac35f5619a59cf06c6979899fb739bb5a99efb
Mahmud2640 <mdmahmud484@gmail.com>
2022-12-12 09:24:59 +0600

    navbar adde

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/Home/About";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
